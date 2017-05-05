b6f4dbd17b7ae77729b1886d0fedc10203210246
flaviofaust <flaviofaust@gmail.com>
2017-05-05 14:31:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,55 +12,96 @@ restService.post('/webhook', function (req, res) {
     console.log('webhook request');
 
     try {
+        var helper = require('sendgrid').mail;
+        var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
         var speech = '';
         var assigened = 'Help Desk';
+        var assigenedMail = 'flavio.faust@pmi.com';
         var name = '';
+        var system = '';
+        var area = '';
+        var email = '';
 
         if (req.body) {
 
             if (req.body.result) {
                 assigened = '';
-                
+
                 switch(req.body.result.action){
                     case 'responsible':
                         name = req.body.result.parameters.Name;
-                        switch(req.body.result.parameters.System){
+                        system = req.body.result.parameters.System;
+                        area = req.body.result.parameters.Area;
+                        switch(system){
                             case 'iSMS':
-                                switch(req.body.result.parameters.Area){
+                                switch(area){
                                     case 'Vendas':
                                         assigened = 'Monique Oliveira';
+                                        assigenedMail = 'flavio.faust@pmi.com';
                                         break;
                                     case 'Operações':
                                         assigened = 'Rogério Pereira';
+                                        assigenedMail = 'flavio.faust@pmi.com';
                                         break;
                                     case 'Trade':
                                         assigened = 'Bruno Perico';
+                                        assigenedMail = 'flavio.faust@pmi.com';
                                         break;
                                     default:
                                         assigened = 'Claudia Laurente';
+                                        assigenedMail = 'flavio.faust@pmi.com';
                                         break;
                                 }
                                 break;
                             case 'BORA':
                                 assigened = 'Romanine';
+                                assigenedMail = 'flavio.faust@pmi.com';
                                 break;
                             case 'TEN':
                             case 'TaskApp':
                                 assigened = 'Alex Wzorek';
+                                assigenedMail = 'flavio.faust@pmi.com';
                                 break;
                             case 'CATES':
                             case 'URE':
                             case 'FPT':
                                 assigened = 'Bruno Perico';
+                                assigenedMail = 'flavio.faust@pmi.com';
                                 break;
                             case 'CIP':
                                 assigened = 'Flavio Faust';
+                                assigenedMail = 'flavio.faust@pmi.com';
                                 break;
                             default:
                                 assigened = 'Help Desk';
+                                assigenedMail = 'flavio.faust@pmi.com';
                                 break;
                         }
                         speech += "Ok " + name + ", estou abrindo uma requisição de suporte em seu nome. O responsável pelo seu problema é " + assigened;
+                        email = "Você tem uma nova requisição de suporte." +
+                                "\nUsuário: " + name +
+                                "\nÁrea: " + area +
+                                "\nSistema: " + system;
+
+                        var from_email = new helper.Email("flaviofaust@gmail.com");
+                        var to_email = new helper.Email(assigenedMail);
+                        var subject = "Requisição de suporte #Slack";
+                        var content = new helper.Content("text/plain", email);
+                        var mail = new helper.Mail(from_email, subject, to_email, content);
+
+                        var request = sg.emptyRequest({
+                          method: 'POST',
+                          path: '/v3/mail/send',
+                          body: mail.toJSON(),
+                        });
+
+                        sg.API(request, function(error, response) {
+                          console.log(response.statusCode);
+                          console.log(response.body);
+                          console.log(response.headers);
+                        });
+
                         break;
                     default:
                     break;
